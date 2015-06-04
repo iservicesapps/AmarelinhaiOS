d1694a9a5b438a73043cce2be6e995b49c9663be
iservicesapps <joao.barata@iservices.pt>
2015-06-04 17:00:58 +0100

    Initial Commit

---
+++ Amarelinha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Amarelinha
+//
+//  Created by Programação 2 on 04/06/15.
+//  Copyright (c) 2015 iServices. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Amarelinha/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Amarelinha
+//
+//  Created by Programação 2 on 04/06/15.
+//  Copyright (c) 2015 iServices. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
